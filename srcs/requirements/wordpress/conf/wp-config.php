812371097abe82c4b3c273d74c801138cfb8c493
jewu <jewu@inception.myguest.virtualbox.org>
2025-06-02 17:00:22 +0200

    feature: Nginx Dockerfile

--- srcs/requirements/wordpress/conf/wp-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
-define('DB_USER', getenv('WORDPRESS_DB_USER') );
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
-define('DB_HOST', getenv('WORDPRESS_DB_HOST') );
-
-define( 'WP_HOME', 'https://jewu.42.fr' );
-define( 'WP_SITEURL', 'https://jewu.42.fr' );
