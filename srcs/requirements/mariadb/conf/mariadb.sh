badc6da5472f0859ebfc47ccf1233effdc5c47e6
jewu <jewu@inception.myguest.virtualbox.org>
2025-06-05 20:09:37 +0200

    wordpress container

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
 #start MariaDB service
-echo -e "\e[33m🚀 Launching mysqld_safe\e[0m"
-/usr/bin/mysqld_safe &
+# echo -e "\e[33m🚀 Launching mysqld_safe\e[0m"
+# /usr/bin/mysqld_safe &
 
-if [ -d "/var/lib/mysql/$SQL_DATABASE" ]
-then
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
     echo "Database already exists"
 else
     #create database and user
     echo -e "\e[33m📊 Database is being created\e[0m"
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-    mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    /usr/bin/mysqld_safe &
+    
+    #wait that MariaDB is ready
+    until mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" ping --silent; do
+        sleep 1
+    done
+
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
 
+    # mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    # mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    # mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
+    # mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    # mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
     #stop the service
-    mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 fi
 
+sleep 5
+
 #launch safe mode MariaDB
+echo -e "\e[33mRelaunching MariaDB\e[0m"
 exec mysqld_safe
