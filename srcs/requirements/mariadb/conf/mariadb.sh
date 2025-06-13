a7352ab5e72d52655152a8de3f952b3b2a72045e
VM Inception <jeanne99@hotmail.fr>
2025-06-13 15:25:02 +0200

    working mariadb when reboot

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,51 +1,8 @@
 #!/bin/sh
 
-# /etc/init.d/mariadb start;
-
-# DB_ALREADY_EXISTS=$(mysql -uroot -p$SQL_ROOT_PASSWORD -e "SHOW DATABASES" | grep $SQL_DATABASE | wc -l);
-
-# if [ $DB_ALREADY_EXISTS -eq 1 ]; then
-# 	echo "Database already exists";
-# else
-# 	echo "Database does not exist";
-# # 	mysql_secure_installation << END
-# # Y
-# # $SQL_ROOT_PASSWORD
-# # $SQL_ROOT_PASSWORD
-# # Y
-# # Y
-# # Y
-# # Y
-# # END
-
-
-# # mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
-
-# # mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;";
-
-# echo "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" | mariadb -u root
-
-# echo "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" | mariadb -u root
-
-# echo "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" | mariadb -u root
-
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" | mariadb -u root
-
-# #refresh
-# echo "FLUSH PRIVILEGES;" | mariadb -u root -p$SQL_ROOT_PASSWORD
-
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-# fi
-
-# sleep 5;
-
-# /etc/init.d/mariadb stop;
-
-# exec "$@";
-
-#################TESTING
-#!/bin/sh
+echo "Fixing permissions on /var/lib/mysql..."
+chown -R mysql:mysql /var/lib/mysql
+chmod 750 /var/lib/mysql
 
 /etc/init.d/mariadb start;
 
@@ -55,21 +12,20 @@ if [ $DB_ALREADY_EXISTS -eq 1 ]; then
 	echo "Database already exists";
 else
 	echo "Database does not exist";
-	mysql_secure_installation << END
-Y
-$SQL_ROOT_PASSWORD
-$SQL_ROOT_PASSWORD
-Y
-Y
-Y
-Y
-END
 
+# mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+# mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;";
 
-mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;";
+echo "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" | mariadb -u root
 
+echo "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" | mariadb -u root
+
+echo "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" | mariadb -u root
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" | mariadb -u root
+
+#refresh
 echo "FLUSH PRIVILEGES;" | mariadb -u root -p$SQL_ROOT_PASSWORD
 
 kill $(cat /var/run/mysqld/mysqld.pid)
@@ -78,40 +34,6 @@ fi
 
 sleep 5;
 
-/etc/init.d/mariadb stop;
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 
 exec "$@";
-
-###MY INITIAL SCRIPT########33
-#!/bin/sh
-
-# /etc/init.d/mariadb start;
-
-# DB_ALREADY_EXISTS=$(mysql -uroot -p$SQL_ROOT_PASSWORD -e "SHOW DATABASES" | grep $SQL_DATABASE | wc -l);
-
-# if [ $DB_ALREADY_EXISTS -eq 1 ]; then
-# 	echo "Database already exists";
-# else
-# 	echo "Database does not exist";
-# 	mysql_secure_installation << END
-# Y
-# $SQL_ROOT_PASSWORD
-# $SQL_ROOT_PASSWORD
-# Y
-# Y
-# Y
-# Y
-# END
-
-
-# mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
-
-# mysql -uroot -p$SQL_ROOT_PASSWORD -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; FLUSH PRIVILEGES;";
-
-# fi
-
-# sleep 1;
-
-# /etc/init.d/mariadb stop;
-
-# exec "$@";
